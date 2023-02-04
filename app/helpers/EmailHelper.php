949c93e26c2f7811fa963fdd83a3c890515a9d6a
David Tuta <santiagotutadiaz@gmail.com>
2023-02-03 19:05:28 -0500

    email

--- app/helpers/EmailHelper.php
+++ app/helpers/EmailHelper.php
@@ -11,7 +11,7 @@ class EmailHelper
         $subject = $razon;
         $for = $email;
         Mail::send($view, $params, function($msj) use($subject,$for){
-            $msj->from("dtuta@engagement.com.co", "Engagement");
+            $msj->from("santiagodiaztuta@gmail.com", "Engagement");
             $msj->subject($subject);
             $msj->to($for);
         });
